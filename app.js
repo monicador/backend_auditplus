b64ce3b499f314316ae9e3a313c24a07f5468948
lamorenove <lamorenove@gmail.com>
2021-10-14 22:35:37 -0500

    Se realizan cambios al archivo por pruebas 1035 pm

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import path from 'path';
-
-
-
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 // Conexión base de datos
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //const uri = 'mongodb://localhost:27017/auditplus';
-const uri = 'mongodb+srv://auditplus:auditplus123@auditplus.x2ndc.mongodb.net/auditplus?retryWrites=true&w=majority';
-const options = {useNewUrlParser: true, useUnifiedTopology: true};
+const url =
+  "mongodb+srv://auditplus:auditplus123@auditplus.x2ndc.mongodb.net/auditplus?retryWrites=true&w=majority";
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // promises
-    mongoose.connect(uri, options).then(
-    () => { console.log('Conectado a DB') },
-    err => { console.log(err) }
+mongoose.connect(url, options).then(
+  () => {
+    console.log("Conectado a DB");
+  },
+  (err) => {
+    console.log(err);
+  }
 );
 
 //MIDDLEWARE
-app.use(morgan('combined'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 //application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }))
-
+app.use(express.urlencoded({ extended: true }));
 
 //rutas
 //app.get('/', function (req, res) {
-  //  res.send('Hello World!');
+//  res.send('Hello World!');
 //});
-app.use('/api', require('./routes/factura'));
-app.use('/api', require('./routes/usuario'));
+app.use("/api", require("./routes/factura"));
+app.use("/api", require("./routes/usuario"));
 
-
-const history = require('connect-history-api-fallback');
+const history = require("connect-history-api-fallback");
 app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //puertos
 //app.listen(3000, function () {
-  //  console.log('Example app listening on port 3000!');
+//  console.log('Example app listening on port 3000!');
 //});
-app.set('puerto', process.env.PORT || 3000);
-app.listen(app.get('puerto'), function () {
-console.log('Example app listening on port'+ app.get('puerto'));
+app.set("puerto", process.env.PORT || 3000);
+app.listen(app.get("puerto"), function () {
+  console.log("Escuchando el puerto" + app.get("puerto"));
 });
